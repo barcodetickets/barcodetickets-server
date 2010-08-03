8c5b09949db85ae2f4b06b9c4c1cbe3e94a0ee3e
Frederick Ding <frederick@frederickding.com>
2010-08-03 21:31:19 +0000

    Added comments and shifted order of methods in Bts_Model_Users. Modified default database config file.

--- application/Exception.php
+++ application/Exception.php
@@ -1,3 +1,10 @@
 <?php
+/**
+ * An exception type used by the BTS application.
+ *
+ * @author	Frederick Ding
+ * @version	$Id$
+ * @see		Zend_Exception
+ */
 class Bts_Exception extends Zend_Exception
 {}
--- application/models/Users.php
+++ application/models/Users.php
@@ -30,11 +30,70 @@ class Bts_Model_Users
 		$this->UsersTable = new Bts_Model_DbTable_Users();
 		$this->PasswordHash = new BtsX_PasswordHash(8, TRUE);
 	}
+	/**
+	 * Checks whether the username is a valid one by checking its length (max.
+	 * in database is 45) and looking for permitted characters (alphanumeric,
+	 * period, hyphen and underscore).
+	 * @param string $username
+	 * @return int
+	 */
 	public function _checkUsername ($username)
 	{
 		if (strlen($username) > 45) return self::USERNAME_TOO_LONG;
 		if (! preg_match('/^[[:alnum:]._-]{1,45}$/', $username)) return self::INVALID_USERNAME;
 		else return self::VALID_USERNAME;
+	}
+	/**
+	 * Changes a given user's password in the database.
+	 *
+	 * @param string $username
+	 * @param string $old_password
+	 * @param string $new_password
+	 * @return boolean true on success and false on failure
+	 */
+	public function changePassword ($username, $old_password, $new_password)
+	{
+		$select = $this->UsersTable->select()->where('username = ?', $username);
+		$row = $this->UsersTable->fetchRow($select);
+		if (! $row instanceof Zend_Db_Table_Row_Abstract) {
+			return false;
+		}
+		if (! $this->PasswordHash->checkPassword($old_password, $row->password)) {
+			return false;
+		}
+		$row->password = $this->PasswordHash->hashPassword($new_password);
+		return (boolean) $row->save();
+	}
+	/**
+	 * Checks whether a given username and password combination is valid.
+	 *
+	 * @param string $username
+	 * @param string $password
+	 * @return boolean
+	 */
+	public function checkPassword ($username, $password)
+	{
+		$select = $this->UsersTable->select()->where('username = ?', $username);
+		$row = $this->UsersTable->fetchRow($select);
+		if (! $row instanceof Zend_Db_Table_Row_Abstract) {
+			return false;
+		}
+		return $this->PasswordHash->checkPassword($password, $row->password);
+	}
+	/**
+	 * Gets the user ID of the user associated with the given username.
+	 *
+	 * @param string $username
+	 * @return int ID if successful or -1 if not found
+	 */
+	public function getUserId ($username)
+	{
+		$select = $this->UsersTable->select(false)->from($this->UsersTable->info('name'), 'user_id')->where('username = ?', $username);
+		$row = $this->UsersTable->fetchRow($select);
+		if (is_null($row)) {
+			return - 1;
+		}
+		return (int) $row->user_id;
 	}
 	/**
 	 * Adds a new user to the BTS users table.
@@ -69,47 +128,23 @@ class Bts_Model_Users
 			'username' => $username ,
 			'password' => $this->PasswordHash->hashPassword($password) ,
 			'email' => $meta['email'] ,
-			'status' => (is_integer($meta['status'])) ? $meta['status'] : 0,
-			'nickname' => ($meta['nickname']) ? $meta['nickname'] : '',
-			'openid' => ($meta['openid']) ? $meta['openid'] : ''
-		);
+			'status' => (is_integer($meta['status'])) ? $meta['status'] : 0 ,
+			'nickname' => ($meta['nickname']) ? $meta['nickname'] : '' ,
+			'openid' => ($meta['openid']) ? $meta['openid'] : '');
 		if (isset($meta['nickname'])) $insertData['nickname'] = $meta['nickname'];
 		$userId = $this->UsersTable->insert($insertData);
 		return $userId;
 	}
-	public function checkPassword ($username, $password)
-	{
-		$select = $this->UsersTable->select();
-		$select->where('username = ?', $username);
-		$row = $this->UsersTable->fetchRow($select);
-		if (! $row instanceof Zend_Db_Table_Row_Abstract) {
-			return false;
-		}
-		return $this->PasswordHash->checkPassword($password, $row->password);
-	}
-	public function changePassword ($username, $old_password, $new_password)
-	{
-		$select = $this->UsersTable->select();
-		$select->where('username = ?', $username);
-		$row = $this->UsersTable->fetchRow($select);
-		if (! $row instanceof Zend_Db_Table_Row_Abstract) {
-			return false;
-		}
-		$row->password = $this->PasswordHash->hashPassword($new_password);
-		return (boolean) $row->save();
-	}
+	/**
+	 * Queries the database to check whether a given username exists.
+	 *
+	 * @param string $username
+	 * @return boolean
+	 */
 	public function userExists ($username)
 	{
-		$select = $this->UsersTable->select(true);
-		$select->where('username = ?', $username);
+		$select = $this->UsersTable->select(true)->where('username = ?', $username);
 		$rows = $this->UsersTable->fetchAll($select);
-		return (boolean) ($rows->count() === 1);
-	}
-	public function getUserId ($username)
-	{
-		$select = $this->UsersTable->select(true);
-		$select->where('username = ?', $username);
-		$row = $this->UsersTable->fetchRow($select);
-		return $row->user_id;
+		return ($rows->count() === 1);
 	}
 }
