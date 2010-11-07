65447aed3d668803b001de1ac8ab611201953876
Frederick Ding <frederick@frederickding.com>
2010-11-07 02:08:24 +0000

    Updated ZF to 1.11.0. Adjusted sales importer model to create new attendees with a SHA1 hex hash instead of decimal (which PHP was converting to a float in scientific notation) when no unique ID is given.

--- application/modules/panel/models/SalesImporter.php
+++ application/modules/panel/models/SalesImporter.php
@@ -40,11 +40,11 @@ class Panel_Model_SalesImporter
 	public function activateTickets ($event, $user, array $importedData)
 	{
 		$log = '';
-		if(!is_numeric($event)) {
+		if (! is_numeric($event)) {
 			throw new Bts_Exception('Event must be a number');
 		}
 		$event = (int) $event;
-		if(!is_numeric($user)) {
+		if (! is_numeric($user)) {
 			throw new Bts_Exception('User ID must be a number');
 		}
 		$user = (int) $user;
@@ -59,7 +59,7 @@ class Panel_Model_SalesImporter
 				if ($exists->count() == 0) {
 					// not found; make one
 					$attendeeId = $this->Attendees->create(
-					$row[1], $row[2], hexdec(sha1($row[1] . ' ' . $row[2])));
+					$row[1], $row[2], 'bts-gen-' . sha1($row[1] . ' ' . $row[2]));
 					if ($attendeeId == - 1) {
 						// failed!
 						$log .= 'Attendee ' . $row[1] .
@@ -67,14 +67,17 @@ class Panel_Model_SalesImporter
 						$log .= 'Ticket ' . $row[0] . " could not be activated.";
 						continue;
 					}
-				} else if($exists->count() >= 1) {
-					// found
-					$attendeeId = $exists[0]->attendee_id;
-				}
+				} else
+					if ($exists->count() >= 1) {
+						// found
+						$attendeeId = $exists[0]->attendee_id;
+					}
 				// now activate tickets
-				$activation = $this->Tickets->activate($event, (int) $row[0], $attendeeId, $user);
-				if($activation != $this->Tickets->getStatusCode('active')) {
- 					$log .= 'Ticket ' . $row[0] . " was not activated successfully. Code $activation\n";
+				$activation = $this->Tickets->activate($event,
+				(int) $row[0], $attendeeId, $user);
+				if ($activation != $this->Tickets->getStatusCode('active')) {
+					$log .= 'Ticket ' . $row[0] .
+					 " was not activated successfully. Code $activation\n";
 				}
 			} else {
 				$log .= "Something went wrong with {$row[0]} {$row[1]} {$row[2]}.\n";
