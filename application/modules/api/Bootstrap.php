04d79f6c16d64ea9c81e792a03c492d9025f424f
Frederick Ding <frederick@frederickding.com>
2013-05-22 12:54:56 -0400

    Disabled display of strict warnings in the API module

--- application/modules/api/Bootstrap.php
+++ application/modules/api/Bootstrap.php
@@ -3,12 +3,21 @@
 /**
  *
  * @author	Frederick Ding
- * @version	$Id$
  * @package	Bts
  */
 class Api_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-
+	/**
+	 * Disables PHP error output
+	 * 
+	 * It is imperative that no PHP-produced errors or warnings interfere with 
+	 * the well-formed XML or JSON response.
+	 */
+	protected function _initPhp ()
+	{
+		ini_set('display_errors', '0'); // turn off output
+		error_reporting(E_ALL | E_STRICT); // log them anyways
+	}
 	protected function _initHelpers ()
 	{
 		Zend_Controller_Action_HelperBroker::addPath(
